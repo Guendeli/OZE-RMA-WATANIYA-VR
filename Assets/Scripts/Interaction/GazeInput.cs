21f81c10def8f5aaecab3b862dea45a7866b6fc5
GUENDELI Omar <guendeliomar@gmail.com>
2017-10-26 17:35:34 +0100

    Basic VR Gaze Input Interaction

---
+++ Assets/Scripts/Interaction/GazeInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInput : MonoBehaviour {
+
+    // public members here
+
+
+    // private members here
+    private Transform _camTransform;
+
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // raycast the shit here
+        RaycastHit hitInfo;
+        if (Physics.Raycast(_camTransform.position, _camTransform.forward, out hitInfo))
+        {
+            InteractiveObject hitObject = hitInfo.collider.GetComponent<InteractiveObject>();
+            if (hitObject != null)
+            {
+                if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+                {
+                    hitObject.OnInteraction();
+                }
+            }
+        }
+        Debug.DrawRay(_camTransform.position, _camTransform.forward, Color.blue);
+	}
+
+    // helper methods are done here
+
+    void Init()
+    {
+        _camTransform = Camera.main.transform;
+    }
+}
---
+++ Assets/Scripts/Interaction/InteractiveObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractiveObject : MonoBehaviour {
+
+
+    // Interactive Object Base Class
+    public UnityEvent onInteraction;
+
+	// Use this for initialization
+    public virtual void OnInteraction()
+    {
+        onInteraction.Invoke();
+    }
+
+    public void DebugInteraction()
+    {
+        // just a fucking test
+        MeshRenderer rend = gameObject.GetComponent<MeshRenderer>();
+        if (rend != null)
+        {
+            rend.enabled = false;
+        }
+    }
+}
